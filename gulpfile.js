ec2f8f97be9171e91b8fe5f5bbee78602027b9a1
Paut991 <ptornero@estudiante.edib.es>
2024-08-01 13:30:09 +0200

    update de gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,16 @@
+// gulpfile.js
+
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
+const cleanCSS = require("gulp-clean-css");
+const rename = require("gulp-rename");
 
 gulp.task("sass", function () {
   return gulp
-    .src("src/sass/**/*.scss")
+    .src("src/sass/main.scss")
     .pipe(sass().on("error", sass.logError))
+    .pipe(cleanCSS())
+    .pipe(rename("main.css"))
     .pipe(gulp.dest("src/css"));
 });
 
